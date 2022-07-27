63028b930f8f3cb9234f8fcade77ca3dd3230d76
JunoField <junofield123@gmail.com>
2022-07-27 17:19:34 +0100

    added empty error for email (as well as wrong format error

--- validation.js
+++ validation.js
@@ -1,38 +1,50 @@
 function validateForm(){
 
     if (document.getElementById("first-name-input").value == "") {
-        showError("first-name");
+        showEmptyError("first-name");
     } else{
-        removeError("first-name");
+        removeEmptyError("first-name");
     }
 
     if (document.getElementById("last-name-input").value == "") {
-        showError("last-name");
+        showEmptyError("last-name");
     } else{
-        removeError("last-name");
+        removeEmptyError("last-name");
     }
 
+    
+    if(document.getElementById("email-input").value == ""){
+        showEmptyError("email");
+        document.getElementById("email-format-error").classList.remove("error-message-active");
+    } else if (!(document.getElementById("email-input").value.match(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/))){
+        document.getElementById("email-format-error").classList.add("error-message-active");
+        document.getElementById("email-input").classList.add("input-error-state");
+        document.getElementById("email-empty-error").classList.remove("error-message-active");
 
-    if (!(document.getElementById("email-input").value.match(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/))){
-        showError("email");
     } else{
-        removeError("email");
+        removeEmptyError("email");
+        document.getElementById("email-format-error").classList.remove("error-message-active");
+
     }
+    
+
 
     if (document.getElementById("password-input").value == "") {
-        showError("password");
+        showEmptyError("password");
     } else{
-        removeError("password");
+        removeEmptyError("password");
     }
 
 }
 
-function showError(caller){
-    document.getElementById(caller + "-error").classList.add("error-message-active");
+
+
+function showEmptyError(caller){
+    document.getElementById(caller + "-empty-error").classList.add("error-message-active");
     document.getElementById(caller + "-input").classList.add("input-error-state");
 }
 
-function removeError(caller){
-    document.getElementById(caller + "-error").classList.remove("error-message-active");
+function removeEmptyError(caller){
+    document.getElementById(caller + "-empty-error").classList.remove("error-message-active");
     document.getElementById(caller + "-input").classList.remove("input-error-state");
 }
