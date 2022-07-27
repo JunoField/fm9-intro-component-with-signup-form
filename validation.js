4c418d7139a0d6bb2e20c4c63b3f6e184394dc9e
JunoField <junofield123@gmail.com>
2022-07-27 16:38:02 +0100

    finished validation, removed console.logs

--- validation.js
+++ validation.js
@@ -1,21 +1,41 @@
 function validateForm(){
     //console.log("button pressed");
 
+    resetErrorStates();
+
     if (document.getElementById("first-name-input").value == "") {
-        console.log("first name empty");
+        showError("first-name");
     }
 
     if (document.getElementById("last-name-input").value == "") {
-        console.log("last name empty");
+        showError("last-name");
     }
 
 
     if (!(document.getElementById("email-input").value.match(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/))){
-        console.log("email not in correct format")
+        showError("email");
     }
 
     if (document.getElementById("password-input").value == "") {
-        console.log("password empty");
+        showError("password");
     }
 
+}
+
+function showError(caller){
+    document.getElementById(caller + "-error").classList.add("error-message-active");
+    document.getElementById(caller + "-input").classList.add("input-error-state");
+}
+
+function resetErrorStates(){
+    document.getElementById("first-name-error").classList.remove("error-message-active");
+    document.getElementById("last-name-error").classList.remove("error-message-active");
+    document.getElementById("email-error").classList.remove("error-message-active");
+    document.getElementById("password-error").classList.remove("error-message-active");
+
+
+    document.getElementById("first-name-input").classList.remove("input-error-state");
+    document.getElementById("last-name-input").classList.remove("input-error-state");
+    document.getElementById("email-input").classList.remove("input-error-state");
+    document.getElementById("password-input").classList.remove("input-error-state");
 }
