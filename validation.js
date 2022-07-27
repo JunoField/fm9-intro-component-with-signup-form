d05e6e3fc34f10e5292f54fb2b9b102156d2820b
JunoField <junofield123@gmail.com>
2022-07-27 16:45:43 +0100

    cleaned up js to avoid redundant resetting of error states

--- validation.js
+++ validation.js
@@ -1,23 +1,28 @@
 function validateForm(){
-    //console.log("button pressed");
-
-    resetErrorStates();
 
     if (document.getElementById("first-name-input").value == "") {
         showError("first-name");
+    } else{
+        removeError("first-name");
     }
 
     if (document.getElementById("last-name-input").value == "") {
         showError("last-name");
+    } else{
+        removeError("last-name");
     }
 
 
     if (!(document.getElementById("email-input").value.match(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/))){
         showError("email");
+    } else{
+        removeError("email");
     }
 
     if (document.getElementById("password-input").value == "") {
         showError("password");
+    } else{
+        removeError("password");
     }
 
 }
@@ -27,15 +32,7 @@ function showError(caller){
     document.getElementById(caller + "-input").classList.add("input-error-state");
 }
 
-function resetErrorStates(){
-    document.getElementById("first-name-error").classList.remove("error-message-active");
-    document.getElementById("last-name-error").classList.remove("error-message-active");
-    document.getElementById("email-error").classList.remove("error-message-active");
-    document.getElementById("password-error").classList.remove("error-message-active");
-
-
-    document.getElementById("first-name-input").classList.remove("input-error-state");
-    document.getElementById("last-name-input").classList.remove("input-error-state");
-    document.getElementById("email-input").classList.remove("input-error-state");
-    document.getElementById("password-input").classList.remove("input-error-state");
+function removeError(caller){
+    document.getElementById(caller + "-error").classList.remove("error-message-active");
+    document.getElementById(caller + "-input").classList.remove("input-error-state");
 }
